f938901fb3c0fedae0b48973d0d1b87918b449c1
DanTheGamerMan <71399229+DanTheGamerMan@users.noreply.github.com>
2021-01-26 13:15:56 -0800

    Add files via upload

---
+++ Fact Finder Project/script.js
@@ -0,0 +1,19 @@
+function showF1(){
+	document.querySelector("#f1").innerHTML = "The first edition of Minecraft released by Notch in 2004 took only 3 days to create!";
+}
+
+function showF2(){
+	document.querySelector("#f1").innerHTML = "There is a term in economics to describe how much happiness an item gives you in relation to it's cost, known as \"utils\"";
+}
+
+function showF3(){
+	document.querySelector("#f1").innerHTML = "The odds of getting a royal flush are exactly 1 in 649,740. <br>	<img src='images/flush.jpg' alt='royalflush'>";
+}
+
+function showF4(){
+	document.querySelector("#f1").innerHTML = "The 100 folds in a chef's hat represent 100 ways to cook an egg.";
+}
+
+function showF5(){
+	document.querySelector("#f1").innerHTML = "Neil Armstrong never said \"That's one small step for man.\"";
+}
